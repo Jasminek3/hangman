c10d814f713224fd60145e5b63a904f7adc4f0bb
Jasmine Kaur <jasminekaur@Jasmines-MacBook-Pro.local>
2022-12-03 20:03:45 +0000

    Adding if statment and validation

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,13 @@
-
+#%%
 import random 
 
 word_list = ['Mango','Banana','Apple','Kiwi','Strawberry']
 
-word = random.choice(word_list)
-print(word)
-
 guess = input("Guess a Letter")
+
+if guess.isalpha() & len(guess) == 1:
+    word = random.choice(word_list)
+    print(word)
+else:
+    print("Make sure your input is an alphabect and only one character")
+# %%
