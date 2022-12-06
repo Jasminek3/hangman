da853d3d7507d2ea08e4db456ac8ae5079707aab
Jasmine Kaur <jasminekaur@Jasmines-MBP.lan>
2022-12-06 18:30:37 +0000

    Add a true condition to while loop

--- milestone_3.py
+++ milestone_3.py
@@ -1,9 +1,8 @@
-
-
 guess = input("Guess a Letter")
 
-while guess.isalpha() & len(guess) == 1:
-    break
-else:
-    print("Invalid letter. Please, enter a single alphabetical character.")
-
+while True:
+    guess = input('Guess a Letter')
+    if guess.isalpha() & len(guess) == 1:
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
