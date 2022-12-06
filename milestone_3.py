ed4c08f8fe2a9f7dda04d0550c07a0912b49b36c
Jasmine Kaur <jasminekaur@Jasmines-MBP.lan>
2022-12-06 19:01:14 +0000

    Adding the ability to guess and display messages accordingly

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,19 @@
-guess = input("Guess a Letter")
+#%%
+import random 
+
+word_list = ['Mango','Banana','Apple','Kiwi','Strawberry']
 
 while True:
     guess = input('Guess a Letter')
     if guess.isalpha() & len(guess) == 1:
-        break
+        word = random.choice(word_list)
+        print(word)
+        if guess in word:
+            print(f'Good guess! "{guess}" is in the word.')
+        else:
+            print(f'Sorry, {guess} is not in the word. Try again.')
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+        break
+
+# %%
