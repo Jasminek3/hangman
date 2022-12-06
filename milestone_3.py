6c5e604c65e13b829486cb3fafa1c4c0e63d4269
Jasmine Kaur <jasminekaur@Jasmines-MacBook-Pro.local>
2022-12-06 23:40:46 +0000

    Adding functions

--- milestone_3.py
+++ milestone_3.py
@@ -1,19 +1,26 @@
-#%%
+
 import random 
 
-word_list = ['Mango','Banana','Apple','Kiwi','Strawberry']
+word_list = ['mango','banana','apple','kiwi','strawberry']
+word = random.choice(word_list)
+print(word)
 
-while True:
-    guess = input('Guess a Letter')
-    if guess.isalpha() & len(guess) == 1:
-        word = random.choice(word_list)
-        print(word)
-        if guess in word:
-            print(f'Good guess! "{guess}" is in the word.')
-        else:
-            print(f'Sorry, {guess} is not in the word. Try again.')
+def check_guess(guess):
+    guess = guess.lower()
+
+    if guess in word:
+        print(f'Good guess! {guess} is in the word')
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-        break
+        print(f'Sorry, {guess} is not in the word. Try again')
 
-# %%
+def asked_for_input():   
+    while True:
+        guess = input('Guess a Letter')
+        if guess.isalpha()==True and len(guess) == 1:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+    check_guess(guess)
+
+asked_for_input() 
