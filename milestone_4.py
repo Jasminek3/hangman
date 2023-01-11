e311d9cdb821b2824e176847c849369731df4971
Jasmine Kaur <jasminekaur@jasmines-mbp.home>
2023-01-11 13:18:22 +0000

    Editing attributes

--- milestone_4.py
+++ milestone_4.py
@@ -1,16 +1,14 @@
 #%%
 import random 
 word_list = ['mango','banana','apple','kiwi','strawberry']
-letter_list = []
 
 class Hangman():
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.word = random.choice(word_list)
-        self.word_guessed = ['_' for letter in self.word]
+        self.word_guessed = list('_' * len(self.word))
+        self.unguessed_letters = len(set(list(self.word)))
         self.num_lives = num_lives
-        self.list_of_guesses = print(letter_list)
-        self.num_letter = 26-(len(letter_list))
-        
+        self.list_letters = []
     
 # %%
