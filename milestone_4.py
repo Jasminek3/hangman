cf93817db7c8c7538174a18ce2ffbe0b8fce7a8f
Jasmine Kaur <jasminekaur@jasmines-mbp.home>
2023-01-11 13:05:35 +0000

    Using classes within hangman, setting up class & attributes

---
+++ milestone_4.py
@@ -0,0 +1,16 @@
+#%%
+import random 
+word_list = ['mango','banana','apple','kiwi','strawberry']
+letter_list = []
+
+class Hangman():
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for letter in self.word]
+        self.num_lives = num_lives
+        self.list_of_guesses = print(letter_list)
+        self.num_letter = 26-(len(letter_list))
+        
+    
+# %%
