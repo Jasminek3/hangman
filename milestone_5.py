1a3da8b3d1f331bbebd5eacde865082041ada880
Jasmine Kaur <jasminekaur@Jasmines-MacBook-Pro.local>
2023-01-12 22:56:06 +0000

    Adding methods

--- milestone_5.py
+++ milestone_5.py
@@ -1,32 +1,16 @@
 #%%
-import random 
+import random  
 word_list = ['mango','banana','apple','kiwi','strawberry']
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.word = random.choice(word_list)
-        self.word_guessed = ['_' * len(self.word)]
+        self.word_guessed = ['_' for letter in self.word]
         self.num_lives = num_lives
         self.list_of_guesses = []
         self.num_letters = len(set(list(self.word)))
 
-        print(f"Unique guesses left: {self.num_letter}")
-
-    def check_guess(self, guess):
-        guess = guess.lower
-        if guess in self.word:
-            print(f'Good guess! {guess} is in the word')
-            for index, char in enumerate(self.word):
-                if char == guess:
-                    self.word_guessed[index] = char
-            print(self.word_guessed) 
-            self.num_letters -= 1
-        else:
-            self.num_letters -= 1
-            print(f'Sorry, {guess} is not in the word.')
-            print(f'You have {self.num_lives} lives left.')
-        self.list_of_guesses.append(guess)
 
     def ask_for_input(self):
         guess = input('Guess a Letter')
@@ -38,5 +22,24 @@ class Hangman:
             else:
                 self.check_guess(guess)
 
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word')
+            for index, char in enumerate(self.word):
+                if char == guess:
+                    self.word_guessed[index] = char
+            print(self.word_guessed) 
+            self.num_letters-=1
+        else:
+            self.num_letters-=1
+            print(f'Sorry, {guess} is not in the word.')
+            print(f'You have {self.num_lives} lives left.')
+        self.list_of_guesses.append(guess)
 
-
+hg = Hangman(word_list)
+print(hg.word)
+print(hg.list_of_guesses)
+print(hg.num_lives)
+hg.ask_for_input()
+# %%
