a9df54fab55fe317413e0b65302e4a3de663008a
Jasmine Kaur <jasminekaur@jasmines-mbp.home>
2023-01-30 16:29:02 +0000

    Initalising the class

--- milestone_4.py
+++ milestone_4.py
@@ -1,14 +1,17 @@
-#%%
 import random 
 word_list = ['mango','banana','apple','kiwi','strawberry']
 
 class Hangman:
+
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.word = random.choice(word_list)
-        self.word_guessed = ['_' * len(self.word)]
+        self.word_guessed = ['_'] * len(self.word)
         self.num_lives = num_lives
         self.list_of_guesses = []
         self.num_letters = len(set(list(self.word)))
 
+myInstance = Hangman(word_list)
+print(len(myInstance.word))
+print(len(myInstance.word_guessed))
 
