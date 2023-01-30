9676079f20ddd4b29eaec853644cfa91084ac7a2
Jasmine Kaur <jasminekaur@jasmines-mbp.home>
2023-01-30 18:45:20 +0000

    Adding a break to a while true loop

--- milestone_4.py
+++ milestone_4.py
@@ -28,6 +28,7 @@ class Hangman:
                 print("You already tried that letter!")
             else:
                 self.check_guess(guess)
+            break
         
 myInstance = Hangman(word_list)
 print(myInstance.word)
